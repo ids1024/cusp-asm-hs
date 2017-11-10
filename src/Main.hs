613ff138849a2ff24e3bf234cde59b0426e75da4
Ian Douglas Scott <ian@iandouglasscott.com>
2017-11-09 23:42:21 -0800

    Print parse errors in saner way

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,11 @@
 import System.IO (hPutStrLn, stderr)
 import System.Exit
+import Control.Exception.Base (displayException)
 import Assemble (assemble)
 
 main :: IO ()
 main = do code <- getContents 
           case assemble code of
-               Left err -> do hPutStrLn stderr (show err)
+               Left err -> do hPutStrLn stderr (displayException err)
                               exitWith $ ExitFailure 1
                Right output -> putStr output
